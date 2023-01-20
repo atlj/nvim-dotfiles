924eebafcb8be969df56f0e4a6947647921b07d5
Burak Güner <burak.guner@callstack.com>
2023-01-20 23:20:12 +0300

    feat: change fuzzy find bindings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,4 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>f', builtin.git_files, {}) -- Respects .gitignore
+vim.keymap.set('n', '<leader>;', builtin.find_files, {}) -- Looks for all files
+vim.keymap.set('n', '<leader>s', builtin.live_grep, {}) -- Searchs for a string, respects .gitignore
