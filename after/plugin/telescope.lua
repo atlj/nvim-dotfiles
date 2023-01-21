b7f05f0617779ac2b6d5448b147ffb68927217b4
Burak Güner <burak.guner@callstack.com>
2023-01-21 17:25:43 +0300

    feat: <C-k> to insert previous search on telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,19 @@
+local telescope = require("telescope")
+local actions = require("telescope.actions")
 local builtin = require('telescope.builtin')
+
+telescope.setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-j>"] = actions.cycle_history_next,
+				["<C-k>"] = actions.cycle_history_prev,
+			},
+			n = { ["q"] = actions.close },
+		}
+	}
+})
+
 vim.keymap.set('n', '<leader>f', builtin.git_files, {}) -- Respects .gitignore
 vim.keymap.set('n', '<leader>;', builtin.find_files, {}) -- Looks for all files
 vim.keymap.set('n', '<leader>s', builtin.live_grep, {}) -- Searchs for a string, respects .gitignore
