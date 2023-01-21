44015c58ce66f06e6e0cf9dfac0410fe4631a1f3
Burak Güner <burak.guner@callstack.com>
2023-01-21 03:29:33 +0300

    feat: toggleterm

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,21 @@
+require("toggleterm").setup {
+	size = 50,
+	direction = "vertical",
+	size = function(term)
+		if term.direction == "horizontal" then
+			return 15
+		elseif term.direction == "vertical" then
+			return vim.o.columns * 0.3
+		end
+	end,
+}
+
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", direction = "float", hidden = true })
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+
+vim.keymap.set("n", "<leader>t", vim.cmd.ToggleTerm)
+vim.keymap.set("n", "<leader>g", function() vim.cmd("lua _lazygit_toggle()") end)
