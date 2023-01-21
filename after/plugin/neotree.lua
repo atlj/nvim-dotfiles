7e95e006d61cad91bee55b39827e932b9ab0ef97
Burak Güner <burak.guner@callstack.com>
2023-01-21 19:59:35 +0300

    feat: show hidden files on neo_tree

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -56,9 +56,9 @@ require("neo-tree").setup({
 				deleted   = "✖", -- this can only be used in the git_status source
 				renamed   = "", -- this can only be used in the git_status source
 				-- Status type
-				untracked = "",
+				untracked = "?",
 				ignored   = "",
-				unstaged  = "",
+				unstaged  = "+",
 				staged    = "",
 				conflict  = "",
 			}
@@ -126,36 +126,15 @@ require("neo-tree").setup({
 	nesting_rules = {},
 	filesystem = {
 		filtered_items = {
-			visible = false, -- when true, they will just be displayed differently than normal items
-			hide_dotfiles = true,
-			hide_gitignored = true,
-			hide_hidden = true, -- only works on Windows for hidden files/directories
-			hide_by_name = {
-				--"node_modules"
-			},
-			hide_by_pattern = { -- uses glob style patterns
-				--"*.meta",
-				--"*/src/*/tsconfig.json",
-			},
-			always_show = { -- remains visible even if other settings would normally hide it
-				--".gitignored",
-			},
-			never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-				--".DS_Store",
-				--"thumbs.db"
-			},
-			never_show_by_pattern = { -- uses glob style patterns
-				--".null-ls_*",
-			},
+			visible = true, -- when true, they will just be displayed differently than normal items
+			hide_dotfiles = false,
+			hide_gitignored = false,
+			hide_hidden = false, -- only works on Windows for hidden files/directories
 		},
-		follow_current_file = false, -- This will find and focus the file in the active buffer every
+		follow_current_file = true, -- This will find and focus the file in the active buffer every
 		-- time the current file is changed while the tree is open.
 		group_empty_dirs = false, -- when true, empty folders will be grouped together
 		hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
-		-- in whatever position is specified in window.position
-		-- "open_current",  -- netrw disabled, opening a directory opens within the
-		-- window like netrw would, regardless of window.position
-		-- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
 		use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
 		-- instead of relying on nvim autocmd events.
 		window = {
