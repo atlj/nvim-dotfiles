f5edaf990a8522f6f3578136ac994aee4be4f843
Burak <md.burak.guner@gmail.com>
2023-01-29 17:57:20 +0300

    feat: make podfiles highlighted as ruby

--- lua/atlj/autocmd.lua
+++ lua/atlj/autocmd.lua
@@ -1,6 +1,5 @@
--- Set the filetype to `ruby` if file ends with `.podspec`
--- Cocoapods spec files end with .podspec
+-- Set the filetype to `ruby` if file ends with `.podspec` or is named `Podfile`
 vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-  pattern = { "*.podspec" },
+  pattern = { "*.podspec", "Podfile" },
   callback = function() vim.opt.filetype = "ruby" end
 })
