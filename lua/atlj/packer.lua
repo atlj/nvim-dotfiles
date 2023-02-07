4283ff4206a3fff92f18bc09360159cc8c67e451
Burak <md.burak.guner@gmail.com>
2023-02-07 21:59:22 +0300

    feat: install packer and dependencies automatically

--- lua/atlj/packer.lua
+++ lua/atlj/packer.lua
@@ -1,4 +1,11 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Install packer if it's not installed already
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+  vim.cmd [[packadd packer.nvim]]
+end
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -73,4 +80,8 @@ return require('packer').startup(function(use)
     "atlj/Pixie.nvim",
     run = function() vim.cmd.PixieInstall() end
   })
+
+  if is_bootstrap then
+    require('packer').sync()
+  end
 end)
