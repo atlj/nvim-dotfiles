dc3c8f19011ddc013c8feea3781d087a701ab8df
Burak Güner <burak.guner@callstack.com>
2024-02-05 16:18:29 +0300

    enable mini align

---
+++ after/plugin/minialign.lua
@@ -0,0 +1,27 @@
+require('mini.align').setup {
+  -- Module mappings. Use `''` (empty string) to disable one.
+  mappings = {
+    start = 'ga',
+    start_with_preview = 'gA',
+  },
+
+  -- Default options controlling alignment process
+  options = {
+    split_pattern = '',
+    justify_side = 'left',
+    merge_delimiter = '',
+  },
+
+  -- Default steps performing alignment (if `nil`, default is used)
+  steps = {
+    pre_split = {},
+    split = nil,
+    pre_justify = {},
+    justify = nil,
+    pre_merge = {},
+    merge = nil,
+  },
+
+  -- Whether to disable showing non-error feedback
+  silent = false,
+}
